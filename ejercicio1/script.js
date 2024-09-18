edd8e317c69cb35410e6452ea1f0b8cf508301b5
Andreafullstack <andreasanjuan21@gmail.com>
2024-09-18 14:40:34 -0300

    trabajo

---
+++ ejercicio1/script.js
@@ -0,0 +1,7 @@
+
+"use strict"
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Gracias por tu mensaje. Nos pondremos en contacto contigo pronto.');
+});
